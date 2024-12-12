9857b693586239ba722f34054e8a9ee8643966d3
Emil Ahlqvist <emil.ahlqvist@caretotranslate.com>
2024-12-12 22:27:25 +0100

    Change switch statement to map for all days

--- main.go
+++ main.go
@@ -16,6 +16,17 @@ import (
 	"github.com/emahl/adventofcode2024/shared"
 )
 
+var days = map[int]func(){
+	1: day1.Run,
+	2: day2.Run,
+	3: day3.Run,
+	4: day4.Run,
+	5: day5.Run,
+	6: day6.Run,
+	7: day7.Run,
+	8: day8.Run,
+}
+
 func main() {
 	chosenDay := 0
 
@@ -26,24 +37,9 @@ func main() {
 		}
 	}
 
-	switch chosenDay {
-	case 1:
-		day1.Run()
-	case 2:
-		day2.Run()
-	case 3:
-		day3.Run()
-	case 4:
-		day4.Run()
-	case 5:
-		day5.Run()
-	case 6:
-		day6.Run()
-	case 7:
-		day7.Run()
-	case 8:
-		day8.Run()
-	default:
+	if runner, exists := days[chosenDay]; exists {
+		runner()
+	} else {
 		fmt.Println("Not implemented yet...")
 	}
 }
